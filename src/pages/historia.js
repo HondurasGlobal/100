2529972b1346a0b2fc0b404e3bdcad7203654fc4
villamilale94 <70185586+villamilale94@users.noreply.github.com>
2020-08-29 22:15:45 -0600

    Commit

--- src/pages/historia.js
+++ src/pages/historia.js
@@ -24,13 +24,7 @@ const Landing = props => (
             <h2>Sed amet aliquam</h2>
           </header>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            magna sed nunc rhoncus condimentum sem. In efficitur ligula tate
-            urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Nullam et orci eu lorem consequat tincidunt vivamus et
-            sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula
-            tate urna.
+           Historia
           </p>
         </div>
       </section>
