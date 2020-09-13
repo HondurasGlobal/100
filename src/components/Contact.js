526dea90a8b426605e2c741dd35ad622f70e0b7c
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2020-09-13 15:37:13 -0600

    Changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,67 +1,201 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Axios from 'axios'
+import aws4 from 'aws-signature-v4';
 
-const Contact = (props) => (
-    <section id="contact">
+class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contactInfo: {
+        name: '',
+        email: '',
+        sendTo: '',
+        message: '',
+      },
+    }
+  }
+
+  handleContactChange = e => {
+    this.setState({
+      contactInfo: {
+        ...this.state.contactInfo,
+        [e.target.name]: e.target.value,
+      },
+    })
+
+    console.log(this.state.contactInfo.sendTo)
+  }
+
+  sendPostRequest = () => {
+
+
+
+    let request = {
+      host: 'dhkqeb8zsi.execute-api.us-east-2.amazonaws.com',
+      method: 'POST',
+      url: 'https://dhkqeb8zsi.execute-api.us-east-2.amazonaws.com/Development/contact',
+      data: this.state.contactInfo, // object describing the foo
+      body: JSON.stringify(this.state.contactInfo), // aws4 looks for body; axios for data  
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+    
+    let signedRequest = aws4.sign(request,
+      {
+        // assumes user has authenticated and we have called
+        // AWS.config.credentials.get to retrieve keys and
+        // session tokens
+        secretAccessKey: AWS.config.credentials.secretAccessKey,
+        accessKeyId: AWS.config.credentials.accessKeyId,
+        sessionToken: AWS.config.credentials.sessionToken
+      })
+    
+    delete signedRequest.headers['Host']
+    delete signedRequest.headers['Content-Length']
+    
+    let info = Axios(signedRequest)
+    console.log(info);
+
+
+
+
+    /*
+    console.log("Its in");
+    var config = {
+      method: 'post',
+      url:
+        'https://dhkqeb8zsi.execute-api.us-east-2.amazonaws.com/Development/contact',
+      headers: {
+        'X-Amz-Date': '20200911T171447Z',
+        Authorization:
+          'AWS4-HMAC-SHA256 Credential=AKIARBGEDMUEYH3PPEV7/20200911/us-east-2/execute-api/aws4_request, SignedHeaders=dropdown;email;host;message;name;x-amz-content-sha256;x-amz-date, Signature=15173f56ed4389e9da9cecadeb99877fb08021e337aeef5dfa3d6defac768574',
+        'Content-Type': 'text/plain',
+      },
+      data: this.state.contactInfo,
+    }
+
+    Axios(config)
+      .then(function(response) {
+        console.log(JSON.stringify(response.data))
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+
+      */
+  }
+
+  render() {
+    return (
+      <section id="contact">
         <div className="inner">
+          <section>
+            <form
+              method="post"
+              onSubmit={this.sendPostRequest}
+            >
+              <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.send}
+                  value={this.name}
+                  required
+                />
+              </div>
+              <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={this.send}
+                  value={this.email}
+                  required
+                />
+              </div>
+              <div className="field">
+                <label htmlFor="dropDown">Dirigido a:</label>
+                <select name="dropDown" required onChange={this.send}>
+                  <option style={{ color: 'black' }} selected>
+                    Selecciona una opción
+                  </option>
+                  <option
+                    style={{ color: 'black' }}
+                    value="maynorx5000@gmail.com"
+                  >
+                    Asociarse
+                  </option>
+                  <option
+                    style={{ color: 'black' }}
+                    value="fialloschris1@gmail.com"
+                  >
+                    Propuestas
+                  </option>
+                  <option style={{ color: 'black' }} value="mayky12@yahoo.com">
+                    Proyectos
+                  </option>
+                  <option style={{ color: 'black' }} value="mdpc5000@gmail.com">
+                    Investigacion
+                  </option>
+                </select>
+              </div>
+              <div className="field">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="6"
+                  onChange={this.send}
+                  value={this.message}
+                ></textarea>
+              </div>
+              <ul className="actions">
+                <li>
+                  <input type="submit" />
+                </li>
+                <li>
+                  <input type="reset" value="Clear" />
+                </li>
+              </ul>
+            </form>
+          </section>
+          <section className="split">
             <section>
-                <form method="post" action="#">
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" onChange={props.send} value={props.name}/>
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" onChange={props.send} value={props.email}/>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="dropDown">Dirigido a:</label>
-                        <select name="dropDown" required onChange={props.send}>
-                            <option defaultValue value="maynorx5000@gmail.com">Asociarse</option>
-                            <option value="fialloschris1@gmail.com">Propuestas</option>
-                            <option value="mayky12@yahoo.com">Proyectos</option>
-                            <option value="mdpc5000@gmail.com">Investigacion</option>
-                            
-                        </select>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6" onChange={props.send} value={props.message}></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" onClick={props.postIt}/></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                    
-                </form>
-                <button onClick={props.postIt}>Test Enviar</button>
+              <div className="contact-method">
+                <span className="icon alt fa-envelope"></span>
+                <h3>Email</h3>
+                <a href="#">information@untitled.tld</a>
+              </div>
             </section>
-            <section className="split">
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
-                    </div>
-                </section>
+            <section>
+              <div className="contact-method">
+                <span className="icon alt fa-phone"></span>
+                <h3>Phone</h3>
+                <span>(000) 000-0000 x12387</span>
+              </div>
             </section>
+            <section>
+              <div className="contact-method">
+                <span className="icon alt fa-home"></span>
+                <h3>Address</h3>
+                <span>
+                  1234 Somewhere Road #5432
+                  <br />
+                  Nashville, TN 00000
+                  <br />
+                  United States of America
+                </span>
+              </div>
+            </section>
+          </section>
         </div>
-    </section>
-)
+      </section>
+    )
+  }
+}
 
 export default Contact
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,6 @@ import Header from './Header'
 import Menu from './Menu'
 import Contact from './Contact'
 import Footer from './Footer'
-import Axios from 'axios'
 
 class Layout extends React.Component {
   constructor(props) {
@@ -15,12 +14,6 @@ class Layout extends React.Component {
       isMenuVisible: false,
       loading: 'is-loading',
       isLanguageSpanish: true,
-      contactInfo: {
-        name: '',
-        email: '',
-        dropDown: '',
-        message: '',
-      },
     }
     this.handleToggleMenu = this.handleToggleMenu.bind(this)
     this.handleLanguageChange = this.handleLanguageChange.bind(this)
@@ -44,57 +37,6 @@ class Layout extends React.Component {
     })
   }
 
-  handleContactChange = e => {
-    this.setState({
-      contactInfo: {
-        ...this.state.contactInfo,
-        [e.target.name]: e.target.value,
-      },
-    })
-
-    console.log(this.state.contactInfo.dropDown)
-  }
-
-  sendPostRequest = () => {
-
-    var config = {
-    method: 'post',
-    url: 'https://dhkqeb8zsi.execute-api.us-east-2.amazonaws.com/Development/contact',
-    headers: {  
-      'X-Amz-Date': '20200911T171447Z', 
-      'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIARBGEDMUEYH3PPEV7/20200911/us-east-2/execute-api/aws4_request, SignedHeaders=dropdown;email;host;message;name;x-amz-content-sha256;x-amz-date, Signature=15173f56ed4389e9da9cecadeb99877fb08021e337aeef5dfa3d6defac768574', 
-      'Content-Type': 'text/plain'
-    },
-    data : this.state.contactInfo
-};
-
-Axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
-    /*
-    const config = {
-      headers: { 
-        'X-Amz-Content-Sha256': 'beaead3198f7da1e70d03ab969765e0821b24fc913697e929e726aeaebf0eba3', 
-        'X-Amz-Date': '20200911T171447Z', 
-        'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIARBGEDMUEYH3PPEV7/20200911/us-east-2/execute-api/aws4_request, SignedHeaders=dropdown;email;host;message;name;x-amz-content-sha256;x-amz-date, Signature=15173f56ed4389e9da9cecadeb99877fb08021e337aeef5dfa3d6defac768574', 
-        'Content-Type': 'text/plain'      
-      },
-    }
-
-    Axios.post(
-      'https://dhkqeb8zsi.execute-api.us-east-2.amazonaws.com/Development/contact',
-      this.state.contactInfo,
-      config
-    ).then(function(response) {
-      console.log(response)
-    })
-    */
-  }
-
   handleLanguageChange() {
     this.setState({
       isLanguageSpanish: !this.state.isLanguageSpanish,
@@ -113,14 +55,7 @@ Axios(config)
         <div id="wrapper">
           <Header onToggleMenu={this.handleToggleMenu} />
           {children}
-          <Contact
-            send={event => this.handleContactChange(event)}
-            postIt={this.sendPostRequest}
-            name={this.state.contactInfo.name}
-            email={this.state.contactInfo.email}
-            sendTo={this.state.contactInfo.sendTo}
-            message={this.state.contactInfo.message}
-          />
+          <Contact/>
           <Footer />
         </div>
         <Menu
